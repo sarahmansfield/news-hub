ffb66f9f3a67fa492decd6d2a6f8636e176e0c77
sarahmansfield <sbmansfield1@gmail.com>
2020-10-17 16:02:23 -0500

    Finished get_top_headlines() function

---
+++ news/api_wrappers.R
@@ -0,0 +1,74 @@
+# packages
+library(httr)
+library(jsonlite)
+library(tidyverse)
+library(purrr)
+
+get_top_headlines <- function(country = "", category = "", sources = "", 
+                              q = "", pageSize = 0, page = 0, apiKey = "") {
+  # check for invalid request parameters
+  if (!all(sapply(list(country, category, sources, q, apiKey), 
+                 is.character))) {
+    stop("country, category, sources, q, and apiKey must be character values")
+  }
+  if (!all(sapply(list(pageSize, page), is.integer))) {
+    stop("pageSize and page must be integer values")
+  }
+  if (country != "" & sources != "") {
+    stop("You can't mix the country and sources parameters")
+  }
+  if (category != "" & sources != "") {
+    stop("You can't mix the category and sources parameters")
+  }
+  if (pageSize < 0 | page < 0) {
+    stop("page and pageSize cannot be negative values")
+  }
+  if (pageSize > 100) {
+    stop("Page size must not exceed 100")
+  }
+  
+  base_url <- "https://newsapi.org/v2/top-headlines"
+  param <- c("country=", "category=", "sources=", "q=", "pageSize=", 
+             "page=", "apiKey=")
+  vals <- c(country, category, sources, q, pageSize, page, apiKey)
+  query <- "?"
+  
+  #build query string
+  for (i in seq_along(param)) {
+    # check for nonempty parameters
+    if (!(param[i] %in% c("pageSize=", "page=")) & vals[i] != "" |
+        (param[i] %in% c("pageSize=", "page=")) & vals[i] > 0) {
+      # check if current query string already has parameters
+      if (nchar(query) > 1) {
+        addQuery <- str_c("&", param[i], vals[i])
+      } else {
+        addQuery <- str_c(param[i], vals[i])
+      }
+      query <- str_c(query, addQuery)
+    }
+  }
+  query <- URLencode(query)
+  
+  fromJSON(str_c(base_url, query)) %>% 
+    as_tibble()
+}
+
+
+get_sources <- function(category, country, apiKey) {
+  base_url <- "https://newsapi.org/v2/sources"
+  query <- "?language=en"
+}
+
+
+get_sentim <- function(x) {
+  # make POST request and save response
+  r <- POST(
+    url    = "https://sentim-api.herokuapp.com/api/v1/",
+    config = add_headers("Accept"       = "application/json",
+                         "Content-Type" = "application/json"),
+    body   = list(text = x),
+    encode = "json")
+  
+  # extract content from POST response
+  content(r, "text", encoding = "UTF-8")
+}
